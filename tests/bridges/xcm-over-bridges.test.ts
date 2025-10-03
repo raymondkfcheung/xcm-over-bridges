51d6bf65d16a4719421a33ef479683650cab4bc4
Raymond Cheung <178801527+raymondkfcheung@users.noreply.github.com>
2025-10-03 15:26:35 +0100

    Add transfer_assets

--- tests/bridges/xcm-over-bridges.test.ts
+++ tests/bridges/xcm-over-bridges.test.ts
@@ -1,12 +1,27 @@
 import { describe, it, expect, beforeAll, afterAll } from "vitest";
-import { createClient } from "polkadot-api";
+import { createClient, Binary, Enum } from "polkadot-api";
+import { getPolkadotSigner } from "polkadot-api/signer";
 import { getWsProvider } from "polkadot-api/ws-provider";
 import { withPolkadotSdkCompat } from "polkadot-api/polkadot-sdk-compat";
 import {
   KusamaBridgeHub,
   PolkadotAssetHub,
   PolkadotBridgeHub,
+  XcmV3MultiassetFungibility,
+  XcmV3WeightLimit,
+  XcmV5Junction,
+  XcmV5Junctions,
+  XcmV5NetworkId,
+  XcmVersionedAssets,
+  XcmVersionedLocation,
 } from "@polkadot-api/descriptors";
+import { sr25519CreateDerive } from "@polkadot-labs/hdkd";
+import {
+  DEV_PHRASE,
+  entropyToMiniSecret,
+  mnemonicToEntropy,
+  ss58Address,
+} from "@polkadot-labs/hdkd-helpers";
 import toHuman from "../../src/helper.js";
 
 const XCM_VERSION = 5;
@@ -27,6 +42,17 @@ async function getSafeXcmVersion(api: any) {
 
 async function getSupportedVersions(api: any) {
   return await api.query.PolkadotXcm.SupportedVersion.getEntries();
+}
+
+function createApiClient(endpoint: string) {
+  return createClient(withPolkadotSdkCompat(getWsProvider(endpoint)));
+}
+
+function deriveAlice() {
+  const entropy = mnemonicToEntropy(DEV_PHRASE);
+  const miniSecret = entropyToMiniSecret(entropy);
+  const derive = sr25519CreateDerive(miniSecret);
+  return derive("//Alice");
 }
 
 function supportsV5ForOtherChain(
@@ -50,15 +76,9 @@ function supportsV5ForOtherChain(
 }
 
 beforeAll(async () => {
-  kusamaBridgeHubClient = createClient(
-    withPolkadotSdkCompat(getWsProvider(KUSAMA_BH)),
-  );
-  polkadotAssetHubClient = createClient(
-    withPolkadotSdkCompat(getWsProvider(POLKADOT_AH)),
-  );
-  polkadotBridgeHubClient = createClient(
-    withPolkadotSdkCompat(getWsProvider(POLKADOT_BH)),
-  );
+  kusamaBridgeHubClient = createApiClient(KUSAMA_BH);
+  polkadotAssetHubClient = createApiClient(POLKADOT_AH);
+  polkadotBridgeHubClient = createApiClient(POLKADOT_BH);
 
   kusamaBridgeHubApi = kusamaBridgeHubClient.getTypedApi(KusamaBridgeHub);
   polkadotAssetHubApi = polkadotAssetHubClient.getTypedApi(PolkadotAssetHub);
@@ -112,5 +132,47 @@ describe("XCM Over Bridges Tests", () => {
     expect(polkadotSaysKusamaBHv5).toBe(true);
   });
 
-  it("transfers across Bridges", async () => {});
+  it("transfers across Bridges", async () => {
+    const alice = deriveAlice();
+    const alicePublicKey = alice.publicKey;
+    const aliceSigner = getPolkadotSigner(
+      alicePublicKey,
+      "Sr25519",
+      alice.sign,
+    );
+    const aliceAddress = ss58Address(alicePublicKey);
+    const origin = Enum("system", Enum("Signed", aliceAddress));
+    const tx: any = polkadotAssetHubApi.tx.PolkadotXcm.transfer_assets({
+      dest: XcmVersionedLocation.V5({
+        parents: 1,
+        interior: XcmV5Junctions.X2([
+          XcmV5Junction.GlobalConsensus(XcmV5NetworkId.Kusama()),
+          XcmV5Junction.Parachain(1000),
+        ]),
+      }),
+      beneficiary: {
+        parents: 0,
+        interior: XcmV5Junctions.X1(
+          XcmV5Junction.AccountId32({
+            id: Binary.fromHex(
+              "0x9818ff3c27d256631065ecabf0c50e02551e5c5342b8669486c1e566fcbf847f",
+            ),
+          }),
+        ),
+      },
+      assets: XcmVersionedAssets.V5([
+        {
+          id: {
+            parents: 1,
+            interior: XcmV5Junctions.Here(),
+          },
+          fun: XcmV3MultiassetFungibility.Fungible(100_000n),
+        },
+      ]),
+      fee_asset_item: 0,
+      weight_limit: XcmV3WeightLimit.Unlimited(),
+    });
+    const extrinsic = await tx.signAndSubmit(aliceSigner);
+    console.log(JSON.stringify(extrinsic, toHuman, 2));
+  });
 });
