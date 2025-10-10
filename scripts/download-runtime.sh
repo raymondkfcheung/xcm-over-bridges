3b80509b5ed61df411a42a829ce2e889efba4a66
Raymond Cheung <178801527+raymondkfcheung@users.noreply.github.com>
2025-10-10 12:01:53 +0100

    Update scripts

--- scripts/download-runtime.sh
+++ scripts/download-runtime.sh
@@ -6,17 +6,37 @@ THIS_DIR=$(cd $(dirname $0); pwd)
 PROJECT_DIR=${THIS_DIR}/../
 cd ${PROJECT_DIR}
 
-if [ ! -f "wasms/bridge_hub_kusama_runtime.compact.compressed.wasm" ]; then
-    echo "Kusama Bridge Hub WASM not found. Downloading..."
-    mkdir -p wasms
-    wget https://github.com/polkadot-fellows/runtimes/releases/download/v1.9.1/compact-wasms.zip -O wasms/runtimes_v1.9.1-wasms.zip
-    unzip wasms/runtimes_v1.9.1-wasms.zip -d wasms/
+# Parachain name, or default to "Kusama Bridge Hub"
+PARACHAIN_NAME="${1:-Kusama Bridge Hub}"
+
+# For WASM filename
+PARACHAIN_UNDERSCORE=$(echo "${PARACHAIN_NAME}" | tr '[:upper:]' '[:lower:]' | tr ' ' '_')
+if [[ "${PARACHAIN_UNDERSCORE}" =~ ^(kusama|polkadot)_ ]] && [[ "${PARACHAIN_UNDERSCORE}" =~ _ ]]; then
+    REST_OF_NAME=$(echo "${PARACHAIN_UNDERSCORE}" | cut -d'_' -f2-)
+    FIRST_WORD=$(echo "${PARACHAIN_UNDERSCORE}" | cut -d'_' -f1)
+    PARACHAIN_UNDERSCORE="${REST_OF_NAME}_${FIRST_WORD}"
 fi
 
-if [ ! -f "configs/kusama-bridge-hub-override.yaml" ]; then
-     echo "Kusama Bridge Hub Config not found. Downloading..."
-     mkdir -p configs
-     wget https://raw.githubusercontent.com/AcalaNetwork/chopsticks/master/configs/kusama-bridge-hub.yml -O configs/kusama-bridge-hub-override.yaml
-     echo "runtime-log-level: 5" >> configs/kusama-bridge-hub-override.yaml
-     echo "wasm-override: wasms/bridge_hub_kusama_runtime.compact.compressed.wasm" >> configs/kusama-bridge-hub-override.yaml
+# For config filename
+PARACHAIN_HYPHEN=$(echo "${PARACHAIN_NAME}" | tr '[:upper:]' '[:lower:]' | tr ' ' '-')
+
+# WASM file path to check and download
+WASM_FILE_PATH="wasms/${PARACHAIN_UNDERSCORE}_runtime.compact.compressed.wasm"
+
+# Config file path to check and download
+CONFIG_FILE_PATH="configs/${PARACHAIN_HYPHEN}-override.yaml"
+
+if [ ! -f "${WASM_FILE_PATH}" ]; then
+    echo "${PARACHAIN_NAME} WASM (${WASM_FILE_PATH}) not found. Downloading..."
+    mkdir -p wasms
+    wget https://github.com/polkadot-fellows/runtimes/releases/download/v1.9.1/compact-wasms.zip -O wasms/runtimes_v1.9.1-wasms.zip
+    unzip -o wasms/runtimes_v1.9.1-wasms.zip -d wasms/
+fi
+
+if [ ! -f "${CONFIG_FILE_PATH}" ]; then
+    echo "${PARACHAIN_NAME} Config (${CONFIG_FILE_PATH}) not found. Downloading..."
+    mkdir -p configs
+    wget https://raw.githubusercontent.com/AcalaNetwork/chopsticks/master/configs/${PARACHAIN_HYPHEN}.yml -O "${CONFIG_FILE_PATH}"
+    echo "runtime-log-level: 5" >> "${CONFIG_FILE_PATH}"
+    echo "wasm-override: wasms/${PARACHAIN_UNDERSCORE}_runtime.compact.compressed.wasm" >> "${CONFIG_FILE_PATH}"
 fi
--- scripts/polkadot/launch.sh
+++ scripts/polkadot/launch.sh
@@ -6,23 +6,11 @@ THIS_DIR=$(cd $(dirname $0); pwd)
 PROJECT_DIR=${THIS_DIR}/../../
 cd ${PROJECT_DIR}
 
-if [ ! -f "wasms/bridge_hub_polkadot_runtime.compact.compressed.wasm" ]; then
-    echo "Polkadot Bridge Hub WASM not found. Downloading..."
-    mkdir -p wasms
-    wget https://github.com/polkadot-fellows/runtimes/releases/download/v1.9.1/compact-wasms.zip -O wasms/runtimes_v1.9.1-wasms.zip
-    unzip wasms/runtimes_v1.9.1-wasms.zip -d wasms/
-fi
-
-if [ ! -f "configs/polkadot-bridge-hub-override.yaml" ]; then
-     echo "Polkadot Bridge Hub Config not found. Downloading..."
-     mkdir -p configs
-     wget https://raw.githubusercontent.com/AcalaNetwork/chopsticks/master/configs/polkadot-bridge-hub.yml -O configs/polkadot-bridge-hub-override.yaml
-     echo "runtime-log-level: 5" >> configs/polkadot-bridge-hub-override.yaml
-     echo "wasm-override: wasms/bridge_hub_polkadot_runtime.compact.compressed.wasm" >> configs/polkadot-bridge-hub-override.yaml
-fi
+${PROJECT_DIR}/scripts/download-runtime.sh "Polkadot Asset Hub"
+${PROJECT_DIR}/scripts/download-runtime.sh "Polkadot Bridge Hub"
 
 echo "Launching Polkadot..."
 npx @acala-network/chopsticks xcm \
     -r polkadot \
-    -p polkadot-asset-hub \
+    -p configs/polkadot-asset-hub-override.yaml \
     -p configs/polkadot-bridge-hub-override.yaml
